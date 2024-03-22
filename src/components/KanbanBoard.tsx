a2fa6eb73133851f28221cfbe0e79ad566b395cb
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-22 07:11:20 +0530

    feat: Initial scaffolding

---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,5 @@
+const KanbanBoard = () => {
+  return <div className="text-blue-200">KanbanBoard</div>
+}
+
+export default KanbanBoard
--- src/data/labels/label.types.ts
+++
@@ -1,6 +0,0 @@
-import { Color } from "../colors/color.types"
-
-export interface Label {
-  title: string
-  color: Color
-}
